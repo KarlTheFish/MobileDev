acde95b166e9eca2f2c18203d6992d4349da5123
Snufkin <90192484+SnufkinTh@users.noreply.github.com>
2023-05-15 13:11:44 +0300

    Firebase score works
    Added firebase scoring system

--- Assets/Scripts/Fishing.cs
+++ Assets/Scripts/Fishing.cs
@@ -7,6 +7,9 @@ public class Fishing : MonoBehaviour
 {
     Vector3 rot;
 
+    public int localScore;
+    public int number;
+
     private bool FishHooked;
     private int FishPulling;
     private GameObject FishObject;
@@ -17,7 +20,22 @@ public class Fishing : MonoBehaviour
         rot = Vector3.zero;
         Input.gyro.enabled = true;
         StartCoroutine(FishingProcess());
+        GetGlobalScore();
     }
+
+    public void GetGlobalScore()
+    {
+        number = AuthManager.instance.globalScore;
+    }
+
+    public void CompareScore()
+    {
+        if (number < localScore)
+        {
+            StartCoroutine(AuthManager.instance.UpdateScore(localScore));
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -42,13 +60,16 @@ public class Fishing : MonoBehaviour
             if(FishPulling > 50) {
                 if (FishPulling < 70) {
                     FishObject = Instantiate(Resources.Load("Fish/fish1") as GameObject);
+                    localScore = Random.Range(1, 40);
                 }
                 else {
                     if (FishPulling < 90) {
                         FishObject = Instantiate(Resources.Load("Fish/fish2") as GameObject);
+                        localScore = Random.Range(40, 80);
                     }
                     else {
                         FishObject = Instantiate(Resources.Load("Fish/fish3") as GameObject);
+                        localScore = Random.Range(80, 100);
                     }
                 }
                 FishHooked = true;
@@ -64,6 +85,7 @@ public class Fishing : MonoBehaviour
         if (FishHooked == true)
         {
             StopCoroutine(FishingProcess());
+            CompareScore();
         }
     }
 
